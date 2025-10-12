e4c40deb331c201c33c9ec945bf4c9ba8f762056
amingdrift <amingdrift@gmail.com>
2025-10-13 01:48:32 +0800

    ci: release

--- artifacts/start_server.bat
+++ artifacts/start_server.bat
@@ -8,7 +8,7 @@ echo Web root: %CD%\out
 echo Port: 3000
 echo ========================================
 
-nodejs\node.exe nodejs/node_modules/.bin/serve -l 3000 out
+nodejs\node.exe nodejs/node_modules/serve/build/main.js -l 3000 out
 
 echo.
 echo Server stopped.
